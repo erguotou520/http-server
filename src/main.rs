a717772061d19860c0e79f1911cfc3cb65230b5e
erguotou <erguotou525@gmail.com>
2022-12-02 14:44:17 +0800

    feat: Add basic logical

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use clap::{arg, Command, Arg};
+use std::path::PathBuf;
+
+use clap::{arg, value_parser, Command};
+
+use crate::server::start_server;
+
+mod server;
 
 fn cli() -> Command {
     Command::new("hs")
@@ -6,33 +12,33 @@ fn cli() -> Command {
         .version("0.0.1")
         .author("erguotou")
         .allow_external_subcommands(true)
-        .subcommand(
-            Command::new("update")
-                .about("Update hs self")
-
-        )
+        .subcommand(Command::new("update").about("Update hs self"))
         .arg(arg!([entryPath] "The entry path to serve"))
-        .arg(Arg::new("host").short('h').long("host").default_value("0.0.0.0"))
-        .arg(Arg::new("port").short('p').long("port").default_value(8080))
-        .arg(Arg::new("gzip").short('g').long("gzip").default_value(true))
-        .arg(Arg::new("open").short('o').long("open").default_value(false))
-        .arg(Arg::new("cache").short('c').long("cache").default_value("1d"))
-        .arg(Arg::new("log").short('l').long("log").default_value("./log"))
-        
+        .arg(arg!(-c --config <FILE> "Config file path").value_parser(value_parser!(PathBuf)))
+        .arg(arg!(-d --host <HOST> "Host to listen on").default_value("0.0.0.0"))
+        .arg(arg!(-p --port <PORT> "Port to listen on").value_parser(value_parser!(u16)).default_value("8080"))
+        .arg(arg!(-g --gzip "Enable gzip").value_parser(value_parser!(bool)).default_value("true"))
+        .arg(arg!(-o --open "Automatically open the browser").value_parser(value_parser!(bool)).default_value("false"))
+        .arg(arg!(-e --cache <CACHE> "Cache duration for static files").default_value("1d"))
+        .arg(arg!(-l --log <PATH> "Path to save log at").default_value("./log"))
 }
 
-fn main() {
+#[actix_web::main]
+async fn main() {
     let matches = cli().get_matches();
     match matches.subcommand() {
-        Some(("update", sub_matches)) => {
-            println!("update")
+        Some(("update", _sub_matches)) => {
+            println!("update");
         }
         Some((ext, sub_matches)) => {
             println!("{:?}, {:?}", ext, sub_matches)
         }
         None => {
-
+            println!("serve");
+            println!("config: {:?}", matches.get_one::<PathBuf>("config"));
+            println!("port: {:?}", matches.get_one::<u16>("port"));
+            println!("gzip: {:?}", matches.get_one::<bool>("gzip"));
+            _ = start_server().await
         }
     }
-
 }
---
+++ src/server.rs
@@ -0,0 +1,22 @@
+use actix_files as fs;
+use actix_web::http::header::{ContentDisposition, DispositionType};
+use actix_web::{App, HttpRequest, HttpServer, get, Error};
+
+#[get("/{filename:.*}")]
+async fn index(req: HttpRequest) -> Result<fs::NamedFile, Error> {
+    let path: std::path::PathBuf = req.match_info().query("filename").parse().unwrap();
+    let file = fs::NamedFile::open(path)?;
+    Ok(file
+        .use_last_modified(true)
+        .set_content_disposition(ContentDisposition {
+            disposition: DispositionType::Attachment,
+            parameters: vec![],
+        }))
+}
+
+pub async fn start_server() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
